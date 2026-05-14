975039f93b4f6f647426d6840a8184c8691e04e4
Zain Marshall <zainmarshall1000@gmail.com>
2026-05-13 20:42:25 -0400

    kitty

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>cp", function()
+  local ok, copilot = pcall(require, "copilot.client")
+  if not ok then return end
+  if copilot.is_disabled() then
+    vim.cmd("Copilot enable")
+    vim.notify("Copilot enabled", vim.log.levels.INFO)
+  else
+    vim.cmd("Copilot disable")
+    vim.notify("Copilot disabled", vim.log.levels.INFO)
+  end
+end, { desc = "Toggle Copilot" })
---
+++ .config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,39 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    opts = {
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+    },
+  },
+  {
+    "saghen/blink.cmp",
+    optional = true,
+    dependencies = { "giuxtaposition/blink-cmp-copilot" },
+    opts = {
+      keymap = {
+        preset = "default",
+        ["<Tab>"] = { "accept", "fallback" },
+        ["<CR>"] = {},
+      },
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer", "copilot" },
+        providers = {
+          copilot = {
+            name = "copilot",
+            module = "blink-cmp-copilot",
+            score_offset = 100,
+            async = true,
+          },
+          snippets = {
+            opts = {
+              search_paths = { vim.fn.stdpath("config") .. "/snippets" },
+            },
+          },
+        },
+      },
+    },
+  },
+}
